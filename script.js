625876ff77fbc6b6a5ac334957408ee7954d1373
devsouzaedu <devsouzaedu@gmail.com>
2023-12-09 12:30:45 -0300

    your message

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona a classe 'show' após 2 segundos
+    setTimeout(function() {
+        document.querySelector('.mashmelo').classList.add('show');
+    }, 2000);
+});
